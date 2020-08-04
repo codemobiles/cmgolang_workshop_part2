c3864a931907b4babf10fb3c80c311888862853f
Chaiyasit Tayabovorn <chaiyasittayabovorn@CMAir.local>
2020-08-04 16:04:23 +0700

    demo2_http_server

---
+++ demo3_gin/index.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Home, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Login, %s, %s", r.URL.Query().Get("username"), r.URL.Query().Get("password"))
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
