9234343b1b7c4eb5f5fcc5aaa3fd85b03f208bf5
codemobiles <chaiyasit.t@gmail.com>
2020-08-18 18:44:00 +0700

    สอน Golang RESTful Ep#7 Gin Logger #3

--- demo4_gin_log/index.go
+++ demo4_gin_log/index.go
@@ -13,11 +13,25 @@ func main() {
 	count := 0
 
 	errlogfile, _ := os.OpenFile(fmt.Sprintf("%s/gin_error.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-	accesslogfile, _ := os.OpenFile(fmt.Sprintf("%s/gin_access.log", runningDir), os.O_CREATE|os.O_WRONLY, 0600)
+	accesslogfile, _ := os.OpenFile(fmt.Sprintf("%s/gin_access.log", runningDir), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
 
 	gin.DefaultErrorWriter = errlogfile
 	gin.DefaultWriter = accesslogfile
-	r.Use(gin.Logger())
+	// r.Use(gin.Logger())
+	// custom format logger
+	r.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
+
+		return fmt.Sprintf("%s  \"%s %s %s %d %s \"%s\" %s\"\n",
+			param.ClientIP,
+			param.Method,
+			param.Path,
+			param.Request.Proto,
+			param.StatusCode,
+			param.Latency,
+			param.Request.UserAgent(),
+			param.ErrorMessage,
+		)
+	}))
 
 	r.GET("/", func(c *gin.Context) {
 		c.Data(200, "text/html; charset=utf-8", []byte("Root"))
@@ -25,13 +39,13 @@ func main() {
 
 	r.GET("/error", func(c *gin.Context) {
 		count = count + 1
-		errlogfile.WriteString(fmt.Sprintf("Error : %d\n", count))
+		errlogfile.WriteString(fmt.Sprintf("Error : %d", count))
 		c.Data(200, "text/html; charset=utf-8", []byte("error"))
 	})
 
 	r.GET("/profile", func(c *gin.Context) {
 		count = count + 1
-		accesslogfile.WriteString(fmt.Sprintf("Count : %d\n", count))
+		accesslogfile.WriteString(fmt.Sprintf("Count : %d", count))
 		c.Data(200, "text/html; charset=utf-8", []byte("profile"))
 	})
 
