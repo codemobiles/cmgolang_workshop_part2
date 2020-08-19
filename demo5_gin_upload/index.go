e9fa141d5a467530aa3de839d887546d37ec04db
codemobiles <chaiyasit.t@gmail.com>
2020-08-19 07:49:58 +0700

    สอน Golang RESTful Ep#8 Gin Upload Single File

--- demo5_gin_upload/index.go
+++ demo5_gin_upload/index.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,12 +12,17 @@ import (
 func main() {
 	router := gin.Default()
 	runningDir, _ := os.Getwd()
+	count := 0
+
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.POST("/upload", func(c *gin.Context) {
 		// single file
+		count = count + 1
 		file, _ := c.FormFile("file")
-		c.SaveUploadedFile(file, fmt.Sprintf("%s/%s", runningDir, file.Filename))
+		extension := filepath.Ext(file.Filename)
+
+		c.SaveUploadedFile(file, fmt.Sprintf("%s/uploaded/%d%s", runningDir, count, extension))
 		c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
 
